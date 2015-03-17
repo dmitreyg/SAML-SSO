65e699ce4e1763803089828de4ef07f854b52aa2
dmitreyg <dmitreyg@yahoo.com>
2015-03-17 14:11:08 -0400

    init

---
+++ RelyingParty1/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Services;
+using System.IdentityModel.Services.Configuration;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.IdentityModel.Claims;
+
+namespace RelyingParty1.Controllers
+{
+    public class UserInfo
+    {
+        public string NameIdentifier { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public List<string> Roles { get; set; }
+        public string EmailAddress { get; set; }
+        public string Sid { get; set; }
+        public List<string> Groups{ get; set; }
+    }
+
+    public class UserController : Controller
+    {
+        public ActionResult Index()
+        {
+            var identity = ((System.Security.Claims.ClaimsIdentity) Thread.CurrentPrincipal.Identity);
+
+            var result = new Dictionary<string, string>();
+            // Access claims
+            var count = 0;
+            foreach (var claim in identity.Claims)
+            {
+                var key = result.ContainsKey(claim.Type) ? claim.Type + (count++).ToString() : claim.Type;
+                result.Add(key, claim.Value);
+            }
+
+            ViewBag.Claims = result;
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            // Load Identity Configuration
+            FederationConfiguration config = FederatedAuthentication.FederationConfiguration;
+
+            // Sign out of WIF.
+            WSFederationAuthenticationModule.FederatedSignOut(new Uri(ConfigurationManager.AppSettings["ida:Issuer"]), new Uri(config.WsFederationConfiguration.Realm));
+
+            return View();
+        }
+    }
+}
---
+++ SingleSignOn/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using SingleSignOn.Security;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Configuration;
+using System.IdentityModel.Services;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SingleSignOn.Controllers
+{
+    public class HomeController : Controller
+    {
+        public const string Action = "wa";
+        public const string SignIn = "wsignin1.0";
+        public const string SignOut = "wsignout1.0";
+
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var action = Request.QueryString[Action];
+
+                if (action == SignIn)
+                {
+                    var principal = (ClaimsPrincipal)User;
+                    var formData = ProcessSignIn(Request.Url, principal);
+                    return new ContentResult() { Content = formData, ContentType = "text/html" };
+                }
+                else if (action == SignOut)
+                {
+                    ProcessSignOut(Request.Url, (ClaimsPrincipal)User, (HttpResponse)HttpContext.Items["HttpResponse"]);
+                }
+            }
+
+            return View();
+        }
+
+        private static string ProcessSignIn(Uri url, ClaimsPrincipal user)
+        {
+            var requestMessage = (SignInRequestMessage)WSFederationMessage.CreateFromUri(url);
+            var signingCredentials = new X509SigningCredentials(CustomSecurityTokenService.GetCertificate(ConfigurationManager.AppSettings["SigningCertificateName"]));
+
+            // Cache?
+            var config = new SecurityTokenServiceConfiguration(ConfigurationManager.AppSettings["IssuerName"], signingCredentials);
+
+            var sts = new CustomSecurityTokenService(config);
+            var responseMessage = FederatedPassiveSecurityTokenServiceOperations.ProcessSignInRequest(requestMessage, user, sts);
+            
+            return responseMessage.WriteFormPost();
+        }
+
+        private static void ProcessSignOut(Uri uri, ClaimsPrincipal user, HttpResponse response)
+        {
+            // Prepare url to internal logout page (which signs-out of all relying parties).
+            string url = uri.OriginalString;
+            int index = url.IndexOf("&wreply=");
+            if (index != -1)
+            {
+                index += 8;
+                string baseUrl = url.Substring(0, index);
+                string wreply = url.Substring(index, url.Length - index);
+
+                // Get the base url (domain and port).
+                string strPathAndQuery = uri.PathAndQuery;
+                string hostUrl = uri.AbsoluteUri.Replace(strPathAndQuery, "/");
+
+                wreply = HttpUtility.UrlEncode(hostUrl + "logout?wreply=" + wreply);
+
+                url = baseUrl + wreply;
+            }
+
+            // Redirect user to logout page (which signs out of all relying parties and redirects back to originating relying party).
+            uri = new Uri(url);
+
+            var requestMessage = (SignOutRequestMessage)WSFederationMessage.CreateFromUri(uri);
+            FederatedPassiveSecurityTokenServiceOperations.ProcessSignOutRequest(requestMessage, user, requestMessage.Reply, response);        }
+    }
+}
